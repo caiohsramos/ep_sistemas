8d428362fbde3683da305c6e6290602789ee9077
igorfratel <igorfratelsantana@gmail.com>
2018-05-12 18:25:52 -0300

    fixed some bugs

--- EP1_9793565_DDL.sql
+++ EP1_9793565_DDL.sql
@@ -3,72 +3,72 @@ CREATE DATABASE yggdrasil;
 
 CREATE TABLE usuario (
   id_usuario SERIAL PRIMARY KEY,
-  usuario_login CHAR(10) NOT NULL UNIQUE,
-  usuario_senha CHAR(256) NOT NULL,
+  usuario_login VARCHAR(10) NOT NULL UNIQUE,
+  usuario_senha VARCHAR(256) NOT NULL,
   usuario_criacao TIMESTAMP NOT NULL
 );
 
 CREATE TABLE perfil (
   id_perfil SERIAL PRIMARY KEY,
-  perfil_codigo CHAR(3) NOT NULL UNIQUE,
-  perfil_nome CHAR(10) NOT NULL UNIQUE
+  perfil_codigo VARCHAR(3) NOT NULL UNIQUE,
+  perfil_nome VARCHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE servico (
   id_servico SERIAL PRIMARY KEY,
-  servico_codigo CHAR(4) NOT NULL UNIQUE,
-  servico_nome CHAR(10) NOT NULL UNIQUE,
-  servico_descricao CHAR(200)
+  servico_codigo VARCHAR(4) NOT NULL UNIQUE,
+  servico_nome VARCHAR(10) NOT NULL UNIQUE,
+  servico_descricao VARCHAR(200)
 );
 
 CREATE TABLE pessoa (
   id_pessoa SERIAL PRIMARY KEY,
-  pessoa_cpf CHAR(11) NOT NULL UNIQUE,
-  pessoa_nome CHAR(70) NOT NULL,
+  pessoa_cpf VARCHAR(11) NOT NULL UNIQUE,
+  pessoa_nome VARCHAR(70) NOT NULL,
   pessoa_data_de_nascimento DATE,
-  pessoa_sexo CHAR(1),
-  pessoa_endereco CHAR(50),
+  pessoa_sexo VARCHAR(1),
+  pessoa_endereco VARCHAR(50),
   pessoa_ID_usuario INT UNIQUE REFERENCES usuario(id_usuario)
 );
 
 CREATE TABLE aluno (
   id_aluno SERIAL PRIMARY KEY,
-  aluno_nusp CHAR(9) NOT NULL UNIQUE,
+  aluno_nusp VARCHAR(9) NOT NULL UNIQUE,
   aluno_ano_de_ingresso INTEGER,  --NÃO BOTEI CONSTRAINT
-  aluno_forma_de_ingresso CHAR(1),
+  aluno_forma_de_ingresso VARCHAR(1),
   aluno_ID_pessoa INT UNIQUE REFERENCES pessoa(id_pessoa)
 );
 
 CREATE TABLE professor (
   id_professor SERIAL PRIMARY KEY,
-  professor_nusp CHAR(9) NOT NULL UNIQUE,
+  professor_nusp VARCHAR(9) NOT NULL UNIQUE,
   professor_ano_de_ingresso INTEGER,  --NÃO BOTEI CONSTRAINT
-  professor_departamento CHAR(20),
+  professor_departamento VARCHAR(20),
   professor_ID_pessoa INT UNIQUE REFERENCES pessoa(id_pessoa)
 );
 
 CREATE TABLE disciplina (
   id_disciplina SERIAL PRIMARY KEY,
-  disciplina_codigo CHAR(7) NOT NULL UNIQUE,
-  disciplina_nome CHAR(50) NOT NULL,
-  disciplina_ementa CHAR(200) NOT NULL,
+  disciplina_codigo VARCHAR(7) NOT NULL UNIQUE,
+  disciplina_nome VARCHAR(50) NOT NULL,
+  disciplina_ementa VARCHAR(200) NOT NULL,
   disciplina_criacao DATE,
-  disciplina_bibliografia CHAR(100),
+  disciplina_bibliografia VARCHAR(100),
   disciplina_credito_aula INTEGER NOT NULL,
   disciplina_credito_trabalho INTEGER NOT NULL,
-  disciplina_tipo CHAR NOT NULL
+  disciplina_tipo VARCHAR NOT NULL
 );
 
 CREATE TABLE modulo (
   id_modulo SERIAL PRIMARY KEY,
-  modulo_nome CHAR(20) NOT NULL UNIQUE,
-  modulo_descricao CHAR(200)
+  modulo_nome VARCHAR(20) NOT NULL UNIQUE,
+  modulo_descricao VARCHAR(200)
 );
 
 CREATE TABLE trilha (
   id_trilha SERIAL PRIMARY KEY,
-  trilha_nome CHAR(20) NOT NULL UNIQUE,
-  trilha_descricao CHAR(200)
+  trilha_nome VARCHAR(20) NOT NULL UNIQUE,
+  trilha_descricao VARCHAR(200)
 );
 
 CREATE TABLE tem_perfil (
@@ -109,5 +109,8 @@ CREATE TABLE cursa (
 
 CREATE TABLE plano (
  id_plano SERIAL PRIMARY KEY,
- plano_ID_cursa INTEGER REFERENCES cursa(id_cursa)
+ plano_nome VARCHAR(20) NOT NULL,
+ plano_ID_cursa INTEGER REFERENCES cursa(id_cursa),
+ plano_semestre INTEGER,
+ plano_ano INTEGER
 );
