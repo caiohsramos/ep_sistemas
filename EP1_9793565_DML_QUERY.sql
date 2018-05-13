d2fcabe18473fe76405466e52d660ad19a43efe3
igorfratel <igorfratelsantana@gmail.com>
2018-05-12 22:55:18 -0300

    vamo porraaa

--- EP1_9793565_DML_QUERY.sql
+++ EP1_9793565_DML_QUERY.sql
@@ -9,3 +9,6 @@ SELECT pessoa_nome FROM pessoa JOIN professor ON id_pessoa=professor_id_pessoa;
 
 -- Descreve as disciplinas que compoe o módulo 'Bitcoin'
 SELECT disciplina_codigo, modulo_nome FROM ((compoe_modulo JOIN disciplina ON compoe_modulo_id_disciplina=id_disciplina) JOIN modulo ON compoe_modulo_id_modulo=id_modulo) WHERE modulo_nome='Bitcoin';
+
+-- Relaciona cada perfil com seu respectivo serviço.
+SELECT perfil_nome, servico_nome FROM ((servico JOIN tem_permissao ON id_servico=tem_permissao_id_servico) JOIN perfil ON id_perfil=tem_permissao_id_perfil) ORDER BY perfil_nome;
